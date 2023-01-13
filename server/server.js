5a7eb01817f2215f243c216f6cc83171da893ec9
imjord <dean.jay.goat@gmail.com>
2023-01-12 18:34:54 -0700

    fixed port like a nub

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 3001;
+const PORT = 3001 || process.env.PORT;
 const CommentRoutes = require('./routes/CommentRoute');
 const PostRoutes = require('./routes/PostRoute');
 const UserRoutes = require('./routes/UserRoute.js');
