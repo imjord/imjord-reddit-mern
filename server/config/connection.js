795ba2940d3f144241b621be00cda9b85c1b86ef
imjord <dean.jay.goat@gmail.com>
2023-01-20 21:46:55 -0700

    hgeroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -5,7 +5,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect("mongodb+srv://imjord:imjord123@cluster0.ny1mkzg.mongodb.net/?retryWrites=true&w=majority");
 // "mongodb://127.0.0.1:27017/imjordreddit"
 
 module.exports = mongoose.connection; 
