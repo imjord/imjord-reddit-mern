5bd269c012f9f0a5c908d9941c5a650be502c48b
imjord <dean.jay.goat@gmail.com>
2023-01-28 00:17:00 -0700

    env idk why heroku is sucking

--- server/config/connection.js
+++ server/config/connection.js
@@ -5,7 +5,10 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 
-mongoose.connect("mongodb+srv://imjord:imjord123@cluster0.ny1mkzg.mongodb.net/?retryWrites=true&w=majority");
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 // "mongodb://127.0.0.1:27017/imjordreddit"
 
 module.exports = mongoose.connection; 
