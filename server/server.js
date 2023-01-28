6f059ea0f5ad6716f359d29cc2e991ad783e4116
imjord <dean.jay.goat@gmail.com>
2023-01-28 14:54:11 -0700

    fixed port

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 3001 || process.env.PORT;
+const PORT = process.env.PORT || 3001;
 const CommentRoutes = require('./routes/CommentRoute');
 const PostRoutes = require('./routes/PostRoute');
 const UserRoutes = require('./routes/UserRoute.js');
