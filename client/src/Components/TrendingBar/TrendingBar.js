b214c5f8db497019fb3b4bc9a9d2fd3b18d262da
imjord <dean.jay.goat@gmail.com>
2023-05-03 00:48:41 -0600

    updated images

--- client/src/Components/TrendingBar/TrendingBar.js
+++ client/src/Components/TrendingBar/TrendingBar.js
@@ -1,40 +1,42 @@
-import React from 'react';
-import './Trending.css';
-import sad1 from './sad.png';
-import sad2 from './n.png';
+import React from "react";
+import "./Trending.css";
+import pic1 from "./man.png";
+import pic2 from "./n.png";
 
 const TrendingBar = () => {
-
   const images = {
-      trending1: {
-        image: sad1
-      },
-      trending2: {
-        image: sad2
-      }
-  }
+    trending1: {
+      image: pic1,
+    },
+    trending2: {
+      image: pic2,
+    },
+  };
   return (
-    <div className='trending-div'>
-        <div className='trending-title'>
-            <h3>Trending today</h3>
-        </div>
-        <div className='trending-card-div'>
-        <div className='trending-card'>
-          
-            <div className='image-caption'>say wat
-            <p>r/ doge and more</p></div>
-          <img src={images.trending1.image} alt="trending image" />
+    <div className="trending-div">
+      <div className="trending-title">
+        <h3>Trending today</h3>
+      </div>
+      <div className="trending-card-div">
+        <div className="trending-card">
+          <div className="image-caption">
+            say wat
+            <p>r/ doge and more</p>
           </div>
-        <div className='trending-card'>
-        <div className='image-caption'> YO
-        <p>r/ doge and more</p></div>
-
-        <img src={images.trending2.image} alt="trending image" />
-
+          <img src={images.trending1.image} alt="trending image" />
         </div>
+        <div className="trending-card">
+          <div className="image-caption">
+            {" "}
+            YO
+            <p>r/xqc and more</p>
+          </div>
+
+          <img src={images.trending2.image} alt="trending image" />
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default TrendingBar
+export default TrendingBar;
